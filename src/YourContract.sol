85bf41d7c8af608dde510656cfea1013cbf4f8d9
lokithe5th <lourenslinde@gmail.com>
2023-10-30 18:22:01 +0200

    Setup

--- src/YourContract.sol
+++ src/YourContract.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 // custom errors
 error TRANSFER_FAILED();
