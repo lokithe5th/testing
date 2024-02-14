7b38894d5c3e4cdf3be8ee37a975b31addcfd1d5
lourens linde <65844331+lokithe5th@users.noreply.github.com>
2024-02-14 11:43:39 +0200

    Tweaks

--- test/Properties.sol
+++
@@ -1,8 +0,0 @@
-
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-import {Asserts} from "@chimera/Asserts.sol";
-import {Setup} from "./Setup.sol";
-
-abstract contract Properties is Setup, Asserts {}
